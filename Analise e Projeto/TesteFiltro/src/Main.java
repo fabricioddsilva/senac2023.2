76998c181b4fc41d2b24764fa738fd2b4fff5264
Fabrício Dias <fabriciodsilva3404@gmail.com>
2023-09-06 10:30:13 -0300

    PWA e Filtro em Java

---
+++ Analise e Projeto/TesteFiltro/src/Main.java
@@ -0,0 +1,20 @@
+
+import java.io.*;
+
+public class Main {
+        public static void main(String[] args) {
+            InputStream inputstream;
+            try {
+                inputstream = new FileInputStream("exemplo.txt");
+                int data = inputstream.read();
+                System.out.println("Conteúdo do arq. exemplo.txt:");
+                while (data != -1) {
+                    System.out.print((char) data);
+                    data = inputstream.read();
+                }
+                inputstream.close();
+            } catch (Exception e1) {   e1.printStackTrace();        }
+        }
+
+
+}
